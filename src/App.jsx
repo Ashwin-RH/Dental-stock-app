9e85107110c10d14550a03720ae12529d17190ed
Ashwin-RH <ashwinharagi@gmail.com>
2026-02-01 21:10:32 +0530

    changes to app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import Home from "./pages/Home";
 export default function App() {
   return (
     <div>
-      <AppLock>
+      {/* <AppLock> */}
       <Home />
-      </AppLock>
+      {/* </AppLock> */}
     </div>
   );
 }
