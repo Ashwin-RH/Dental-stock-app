88c62b1c031a152b85732f4d1dc6489f86213acc
Ashwin-RH <ashwinharagi@gmail.com>
2026-02-05 18:24:38 +0530

    production issues

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBCF5RzPct1Eegu4IREYe2zskEbgKgn4Pk",
-  authDomain: "sam-stock-manager.firebaseapp.com",
-  projectId: "sam-stock-manager",
-  storageBucket: "sam-stock-manager.firebasestorage.app",
-  messagingSenderId: "550813447139",
-  appId: "1:550813447139:web:9f39933962354ddb23e6d6"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// ✅ THIS LINE FIXES EVERYTHING
 export const db = getFirestore(app);
